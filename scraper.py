8736d1f4393c9acbb134ef2a4de777a3ff898050
jasoncwells <jasoncarlwells@yahoo.co.uk>
2016-01-21 09:31:09 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,11 @@
 #
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
-#
+myvar = 'What is happening today'
+myage = 46
+mylist = ['how','now','brown','cow'
+mynumlist = [1, 2, 3, 4, 5]
+listlength = len(mylist)
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
